ca0547388ab73b4d596aeef66451f483fb0e6c3d
Ryan Lewis <ryansname@gmail.com>
2026-03-09 15:38:55 +1300

    Remove battery-control, replaced by powerctl repo

--- battery-control/derivation.nix
+++
@@ -1,7 +0,0 @@
-{ buildGoModule }:
-buildGoModule {
-  pname = "battery-control";
-  version = "1.0";
-  src = ./.;
-  vendorHash = "sha256-CMi8SaInt9dsaHvy3sxTLjIB10dajvi0xA92QMo+whQ=";
-}
--- battery-control/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/ryansname/powerhouse/battery-control/control"
-)
-
-func main() {
-	control.Run()
-}
